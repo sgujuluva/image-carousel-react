6b83cde5e86579127fa265e9356dcf1b0a5def0f
sgujuluva <sanghee04@gmail.com>
2022-07-20 14:49:20 +0200

    comments added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//importing immges
+//importing images
 import photo1 from "./images/photo1.png";
 import photo2 from "./images/photo2.png";
 import photo3 from "./images/photo3.png";
@@ -19,31 +19,35 @@ const images = [
   { original: photo6, id: 6 },
 ];
 function App() {
+  //set the usestate for id and checking the images
   const [currentId, setCurrentId] = useState(1);
-
+//find the current image using find array method
   let currentImage = images.find((photos) => photos.id === currentId);
 
   const handleNext = () => {
     if (currentId < images.length) {
-      setCurrentId(currentId + 1);
+      setCurrentId(currentId + 1); // increment the id by 1 whenenver next button is clicked
     } else {
-      setCurrentId(1);
+      setCurrentId(1); // after images length , reset t the initial image
     }
   };
 
   const handlePrevious = () => {
     if (currentId >1){
-      setCurrentId(currentId - 1);
+      setCurrentId(currentId - 1); // whenever the previous button is clicked decrement by 1
     }else{
-      setCurrentId(5);
+      setCurrentId(5); // when the id = 1, it should be resetted to the last image with id=5
     }
   }
   return (
     <div className="App">
+      <div class="container">
       <h1>Image Carousel</h1>
       {<img width="500" height="500" src={currentImage.original} alt="" />}
       <button onClick={handleNext}>Next</button>
       <button onClick={handlePrevious}>Previous</button>
+      </div>
+      
     </div>
   );
 }
