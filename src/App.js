4a7a01d83c145168e8476cc828c0adbb1a8873bd
sgujuluva <sanghee04@gmail.com>
2022-07-20 11:23:06 +0200

    photos added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 //importing immages
-import photo1 from "./images"
+import photo1 from "./images/photo1.png"
+import photo2 from "./images/photo2.png"
+import photo3 from "./images/photo3.png"
+import photo4 from "./images/photo4.png"
+import photo5 from "./images/photo5.png"
 //styles
 import './App.css';
 //intialisation images
 const images = [
-{
-original : "photo1.png"
-}
+{original : photo1},
+{original : photo2},
+{original : photo3},
+{original : photo4},
+{original : photo5}
 ]
 function App() {
   return (
     <div className="App">
      <h1>Image Carousel</h1>
-   {  images.map(item => <div>{item.original}</div>)}
+   {  images.map(item => <img src={item.original}/>)}
     
     </div>
   );
