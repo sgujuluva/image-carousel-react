e686970510efc856315d18ce92548af83f7d67d8
sgujuluva <sanghee04@gmail.com>
2022-07-20 14:39:31 +0200

    find the current image using find method

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import photo2 from "./images/photo2.png";
 import photo3 from "./images/photo3.png";
 import photo4 from "./images/photo4.png";
 import photo5 from "./images/photo5.png";
+import photo6 from "./images/photo6.png";
 //lib
 import { useState } from "react";
 //styles
@@ -15,31 +16,24 @@ const images = [
   { original: photo3, id: 3 },
   { original: photo4, id: 4 },
   { original: photo5, id: 5 },
+  { original: photo6, id: 6 },
 ];
 function App() {
-  const [currentImage, setCurrentImage] = useState(images[0].original);
-  let index = 0;
-  const handleNext = () => {
-    if (index < images.length) {
-      index = index + 1;
-      setCurrentImage(images[index].original);
-    }
-    //setCurrentImage(images[0].original);
-  };
-  const handlePrevious = () => {
-    if (index < images.length && index > 0) {
-      index = index - 1;
-      console.log(index)
-      setCurrentImage(images[index].original);
-      
-    }
-  };
+ 
+  const [currentId, setCurrentId] = useState(1);
+
+  let currentImage = images.find(photos => photos.id === currentId);
+
+const handleNext = () => {
+  
+}
+ 
   return (
     <div className="App">
       <h1>Image Carousel</h1>
-      {<img src={currentImage} />}
-      <button onClick={handleNext}>Next</button>
-      <button onClick={handlePrevious}>Previous</button>
+      {<img width ="500" height="500" src = {currentImage.original}  alt = ""/>}
+      <button  onClick={handleNext} >Next</button>
+      <button /* onClick={handlePrevious} */>Previous</button>
     </div>
   );
 }
